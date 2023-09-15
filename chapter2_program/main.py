e577802828510852c4dee365f24958d0d39be48b
Cameron Angliss <cameronangliss99@gmail.com>
2023-09-14 23:46:16 -0500

    remove all prints

--- chapter2_program/main.py
+++ chapter2_program/main.py
@@ -65,19 +65,16 @@ class ConstStepSizeAgent:
 
 
 def train(agent_class):
-    print(f"Training {agent_class.__name__}...")
     reward_history = [0] * 10**4
     optimal_action_ratio_history = [0] * 10**4
     for run_num in range(300):
         agent = agent_class()
         for step_num in range(10**4):
-            print(f"Progress: {round(100 * ((10**4 * run_num) + step_num) / (300 * 10**4), ndigits=2)}%", end="\r")
             optimal_action = agent.get_optimal()
             action, reward = agent.choose()
             reward_history[step_num] = incremental_average(reward_history[step_num], reward, run_num)
             action_was_optimal = action == optimal_action
             optimal_action_ratio_history[step_num] = incremental_average(optimal_action_ratio_history[step_num], float(action_was_optimal), run_num)
-    print("Done!")
     return reward_history, optimal_action_ratio_history
 
 
