20453e25a04a9ad6c78676b401fe3f7e2cb74706
Cameron Angliss <cameronangliss99@gmail.com>
2023-09-20 01:18:14 -0500

    trying to get a fix

--- chapter2_program/main.py
+++ chapter2_program/main.py
@@ -14,43 +14,38 @@ class Arm:
         self.mean += np.random.normal(0, 0.01)
 
 
-# NOTE: We assume that n begins at 0
-def incremental_average(current_average: float, new_value: float, n: int) -> float:
-    return (n / (n + 1)) * current_average + new_value / (n + 1)
-
-
-class SampleAverageAgent:
+class Agent:
     def __init__(self):
         self.arms = [Arm() for _ in range(10)]
-        self.avg_rewards = [0] * 10
+        self.q_estimates = [0] * 10
         self.epsilon = 0.1
-        self.n = 0
-
+    
     def get_optimal(self):
         return np.argmax([arm.mean for arm in self.arms])
+
+
+class SampleAverageAgent(Agent):
+    def __init__(self):
+        super().__init__()
+        self.n = 0
 
     def choose(self):
         if np.random.random() < self.epsilon:
             action = np.random.choice(range(10))
         else:
-            action = np.argmax(self.avg_rewards)
+            action = np.argmax(self.q_estimates)
         reward = self.arms[action].pull()
-        self.avg_rewards[action] = incremental_average(self.avg_rewards[action], reward, self.n)
+        self.n += 1
+        self.q_estimates[action] += (reward - self.q_estimates[action]) / self.n
         for arm in self.arms:
             arm.update()
-        self.n += 1
         return action, reward
 
 
-class ConstStepSizeAgent:
+class ConstStepSizeAgent(Agent):
     def __init__(self):
-        self.arms = [Arm() for _ in range(10)]
-        self.q_estimates = [0] * 10
+        super().__init__()
         self.alpha = 0.1
-        self.epsilon = 0.1
-
-    def get_optimal(self):
-        return np.argmax([arm.mean for arm in self.arms])
 
     def choose(self):
         if np.random.random() < self.epsilon:
@@ -74,9 +69,8 @@ def train(agent_class):
         for step_num in range(10**4):
             optimal_action = agent.get_optimal()
             action, reward = agent.choose()
-            reward_history[step_num] = incremental_average(reward_history[step_num], reward, run_num)
-            action_was_optimal = action == optimal_action
-            optimal_action_ratio_history[step_num] = incremental_average(optimal_action_ratio_history[step_num], float(action_was_optimal), run_num)
+            reward_history[step_num] += (reward - reward_history[step_num]) / (run_num + 1)
+            optimal_action_ratio_history[step_num] += (float(action == optimal_action) - optimal_action_ratio_history[step_num]) / (run_num + 1)
     print("Done!")
     return reward_history, optimal_action_ratio_history
 
