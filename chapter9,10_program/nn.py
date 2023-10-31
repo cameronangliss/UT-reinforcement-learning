54552a0235b293fa50d162f18c91c15d4d0dd88c
Cameron Angliss <cameronangliss99@gmail.com>
2023-10-31 17:03:57 -0500

    initial attempt (not working yet

--- chapter9,10_program/nn.py
+++ chapter9,10_program/nn.py
@@ -1,7 +1,7 @@
 import numpy as np
 from algo import ValueFunctionWithApproximation
 
-import tensorflow as tf
+import torch
 
 class ValueFunctionWithNN(ValueFunctionWithApproximation):
     def __init__(self,
@@ -9,13 +9,24 @@ class ValueFunctionWithNN(ValueFunctionWithApproximation):
         """
         state_dims: the number of dimensions of state space
         """
-        # TODO: implement this method
+
+        self.network = torch.nn.Sequential(
+            torch.nn.Linear(state_dims, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 1)
+        )
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-3, betas=(0.9, 0.999))
 
     def __call__(self,s):
-        # TODO: implement this method
-        return 0.
+        # print("state:", s)
+        return float(self.network(torch.tensor(s))[0].item())
 
     def update(self,alpha,G,s_tau):
-        # TODO: implement this method
-        return None
-
+        # print(s_tau)
+        loss = G - self(s_tau)
+        # print(loss)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
