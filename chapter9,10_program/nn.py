f04aea55549865bb743d4fa5ca885d9b7c93de2d
Cameron Angliss <cameronangliss99@gmail.com>
2023-11-01 20:56:48 -0500

    bugfixes

--- chapter9,10_program/nn.py
+++ chapter9,10_program/nn.py
@@ -10,7 +10,7 @@ class ValueFunctionWithNN(ValueFunctionWithApproximation):
         state_dims: the number of dimensions of state space
         """
 
-        self.loss = torch.nn.MSELoss()
+        self.loss_fn = torch.nn.MSELoss()
         self.network = torch.nn.Sequential(
             torch.nn.Linear(state_dims, 32),
             torch.nn.ReLU(),
@@ -18,18 +18,16 @@ class ValueFunctionWithNN(ValueFunctionWithApproximation):
             torch.nn.ReLU(),
             torch.nn.Linear(32, 1)
         )
-        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-3, betas=(0.9, 0.999), weight_decay=1e-4)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.network.to(device=self.device)
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-3, betas=(0.9, 0.999))
 
     def __call__(self,s):
         self.network.eval()
-        input_tensor = torch.tensor(s, dtype=torch.float).to(self.device)
-        return float(self.network(input_tensor)[0].item())
+        input_tensor = torch.tensor(s, dtype=torch.float)
+        return float(self.network(input_tensor)[0].detach().item())
 
     def update(self,alpha,G,s_tau):
         self.network.train()
-        loss = self.loss(torch.tensor([self(s_tau)], requires_grad=True), torch.tensor([G], requires_grad=True))
+        loss = self.loss_fn(self.network(torch.tensor(s_tau)), torch.tensor([G]))
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
